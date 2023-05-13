343d0f9219cc99c7827f0c116d90381ca3814c88
Luis Antonio Zambrano Demera <76109631+lzambran@users.noreply.github.com>
2023-05-12 21:59:13 -0500

    update

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const usuarioAxios = axios.create({
-    baseURL: 'http://34.123.205.43/'
+    //baseURL: 'http://34.123.205.43/'
+    baseURL: 'http://localhost:5000'
 })
 
 export default usuarioAxios;
